af102e7fd8fef7011f14c4326af099614bfdc9ff
hilmisidek <zalikamari@gmail.com>
2021-11-30 16:56:21 +0800

    Using Assert method in NUnit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Ocelot.Infrastructure;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 using System.Globalization;
-
+using NUnit.Framework;
 namespace carlist
 {
     class Program
@@ -12,39 +10,25 @@ namespace carlist
         static void Main(string[] args)
         {
             String harga = "RM000";
-            
+
             IWebDriver driver = new ChromeDriver("C:\\Driver");
             driver.Url = "https://carlist.my/"; // This will open up the URL
             driver.FindElement(By.XPath("//form/div/div/div")).Click(); //click on dropdown list
             driver.FindElement(By.XPath("//div[contains(@data-value,'used')]")).Click(); //select "used"
             driver.FindElement(By.CssSelector(".search-button > .btn")).Click(); //click on search button
 
-           // Wait.until(ExpectedConditions.visibilityOf(driver.FindElement(By.XPath("//*[@id='classified-listings-result']/article[1]/div/div/h2/a"))));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                        
             driver.FindElement(By.XPath("//*[@id='classified-listings-result']/article[1]/div/div/h2/a")).Click(); //click on 1st result
-            harga=driver.FindElement(By.XPath("//div[starts-with(@class,'listing__price')]")).Text; //capture the price
+            harga = driver.FindElement(By.XPath("//div[starts-with(@class,'listing__price')]")).Text; //capture the price
             String[] hargadua = harga.Split(" "); //split RM with number
             String hargaFinal = hargadua[1]; //take the second portion of the split
             int hargaInt = int.Parse(hargaFinal, NumberStyles.AllowThousands); //convert to integer recognize thousand separator
-            Console.Out.WriteLine("Car Price : " + hargaInt); // print the captured price in integer
-            int priceCompare = 1000; //value to assert price more than the value
-            String result = assertion(hargaInt, priceCompare);
-            Console.Out.WriteLine(result); //print output
+            Console.Out.WriteLine("Car Price :" + harga); // print the captured price in integer 
+            Assert.That(hargaInt, Is.GreaterThan(1000));
+            Console.Out.WriteLine("Passed: Price is more than RM1,000"); //print output
             driver.Close();
             return;
         }
-
-        static String assertion(int hargaFound, int valueAsssert)
-        {
-            if (hargaFound > valueAsssert)
-            {
-                return "Passed : Car Price is More Than RM1000";
-            }
-            else
-            {
-                return "Failed : Car Price is Not More Than RM1000";
-            }
-        }
-
-
     }
 }
