d99bd8ea9544d71213486287ee264eda86d2ce06
hilmisidek <zalikamari@gmail.com>
2021-12-01 10:19:22 +0800

    separate method for webelement

--- Program.cs
+++ Program.cs
@@ -9,30 +9,48 @@ namespace carlist
     {
         static void Main(string[] args)
         {
-            String harga = "RM000";
+            IWebDriver Driver = new ChromeDriver("C:\\Driver");
+            String webURL= "https://carlist.my/"; //initialize web address
 
-            IWebDriver driver = new ChromeDriver("C:\\Driver");
-            driver.Url = "https://carlist.my/"; // This will open up the URL
+            String harga = GetPriceText(webURL,Driver); //call method to get price in string
+            
+            Assert.That(harga, Does.StartWith("RM"),"Blocked no price"); //test the price, sometime 1st result show no price, only "call now"                     
+            Console.Out.WriteLine("\nCar Price :" + harga); // print the captured price in String 
+            
+            int hargaIntFinal = GetHarga(harga); //call method to convert Price to Integer
+            
+            Assert.That(hargaIntFinal, Is.GreaterThan(1000)); //Assert price more than 1000
+            Console.Out.WriteLine("Passed: Price is more than RM1,000"); //print output
+
+            Driver.Close(); //close webdriver
+
+            return; //stop execution
+        }
+
+        
+        static String GetPriceText(String webURL, IWebDriver driver)
+        { 
+            String hargaText = "RM000"; //initialize
+        
+            //get all required element
+            driver.Url = webURL; // This will open up the URL
             driver.FindElement(By.XPath("//form/div/div/div")).Click(); //click on dropdown list
             driver.FindElement(By.XPath("//div[contains(@data-value,'used')]")).Click(); //select "used"
             driver.FindElement(By.CssSelector(".search-button > .btn")).Click(); //click on search button
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); //wait for the element
             driver.FindElement(By.XPath("//*[@id='classified-listings-result']/article[1]/div/div/h2/a")).Click(); //click on 1st result
-            harga = driver.FindElement(By.XPath("//div[starts-with(@class,'listing__price')]")).Text; //capture the price
-            Assert.That(harga, Does.StartWith("RM"),"Blocked no price"); //test the price, sometime 1st result show no price                     
-            Console.Out.WriteLine("Car Price :" + harga); // print the captured price in integer 
-            int hargaIntFinal = GetHarga(harga); //call method to convert Price to Integer
-            Assert.That(hargaIntFinal, Is.GreaterThan(1000)); //Assert price more than 1000
-            Console.Out.WriteLine("Passed: Price is more than RM1,000"); //print output
-            driver.Close();
-            return;
+        
+            hargaText = driver.FindElement(By.XPath("//div[starts-with(@class,'listing__price')]")).Text; //capture the price
+        
+            return hargaText; //return price in String
         }
-        static int GetHarga(String harga)
+
+        static int GetHarga(String harga) //method to covert string to integer
         {
             String[] hargadua = harga.Split(" "); //split RM with number
             String hargaFinal = hargadua[1]; //take the second portion of the split
             int hargaInt = int.Parse(hargaFinal, NumberStyles.AllowThousands); //convert to integer recognize thousand separator
-            return hargaInt;
+            return hargaInt; //return final integer
         }
 
 
